1e86821aae89970a76b3c724e5f8a4a13cbca80d
Aline de Lima Santos <linsantos93@gmail.com>
2018-10-22 22:23:43 -0300

    Código com Babel, JSX e React

--- Projeto/scripts.js
+++ Projeto/scripts.js
@@ -1,29 +1,15 @@
-// <a class="link" href="/conta">Criar uma conta</a>
+const linkCriarUmaConta = <a class="link" href="/conta">Criar uma conta</a>
+const linkFazerLogin = <a class="link" href="/conta">Fazer login</a>
 
-const linkCriarUmaConta = React.createElement("a", {
-    className: "link", 
-    href: "/conta", 
-    children: "Criar uma conta"
-});
+const formularioLogin = (
+    <form>
+        {[linkCriarUmaConta, linkFazerLogin]}
+    </form>
+)
 
-//  <a class="link" href="/conta">Fazer login</a>
+const divisaoProjeto = document.getElementById("projeto");
 
-const linkFazerLogin = React.createElement("a", {
-    className: "link", 
-    href:"/login", 
-    children: "Fazer login"
-});
-
-//div onde serão posicionadas as "children"
-
-const formularioLogin = React.createElement("form",{
-    children: [linkCriarUmaConta, linkFazerLogin]
-});
-
-const divisaoProjeto = document.getElementById("projeto"); // essa div faz parte da DOM REAL
-
-//ReactDOM.render(elemento React, elemento do DOM real)
-ReactDOM.render(formularioLogin, divisaoProjeto) // fala qual é o elemento react que voce quer colocar no dom real
+ReactDOM.render(formularioLogin, divisaoProjeto)
 
 
 
