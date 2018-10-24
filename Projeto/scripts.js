8e5a3665ea369fd89c9587c10ba318f404b56f28
Aline de Lima Santos <linsantos93@gmail.com>
2018-10-23 21:19:31 -0300

    Criando primeiras componentes com JSX

--- Projeto/scripts.js
+++ Projeto/scripts.js
@@ -1,15 +1,50 @@
-const linkCriarUmaConta = <a class="link" href="/conta">Criar uma conta</a>
-const linkFazerLogin = <a class="link" href="/conta">Fazer login</a>
+function Link(props){
+    return (
+        <a className="link" href={props.url}>
+            {props.children}
+        </a>
+    );
+};
 
-const formularioLogin = (
-    <form>
-        {[linkCriarUmaConta, linkFazerLogin]}
-    </form>
-)
+function Botao(props){
+    let classes = "botao";
 
+    if (props.desabilitado){
+        classes += " botao--desabilitado";
+    };
+
+    return (
+        <input className={classes} type="submit" value={props.children} />
+    );
+};
+
+const paginaLogin = (
+    <main className="pagina-login">
+        <h1>Login</h1>
+        <p>Entre com seu e-mail e senha.</p>
+        <Link url="/conta">Criar uma conta</Link>
+        <Link url="/login">Fazer login</Link>
+        <Botao desabilitado>Enviar</Botao>
+        <Botao>Enviar</Botao>
+    </main>
+);
+
+const props = {url: "/conta", children: "Criar uma conta"}
 const divisaoProjeto = document.getElementById("projeto");
+ReactDOM.render(paginaLogin, divisaoProjeto)
 
-ReactDOM.render(formularioLogin, divisaoProjeto)
+// const linkCriarUmaConta = <a class="link" href="/conta">Criar uma conta</a>;
+// const linkFazerLogin = <a class="link" href="/conta">Fazer login</a>;
+
+// const formularioLogin = (
+//     <form>
+//         {[linkCriarUmaConta, linkFazerLogin]}
+//     </form>
+// );
+
+// const divisaoProjeto = document.getElementById("projeto");
+
+// ReactDOM.render(formularioLogin, divisaoProjeto)
 
 
 
