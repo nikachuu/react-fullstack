66970ab65dbb44c2fd8e8629ac98c776642e746a
Aline de Lima Santos <linsantos93@gmail.com>
2018-10-24 21:33:05 -0300

    Separação de componentes

--- Projeto/src/index.js
+++ Projeto/src/index.js
@@ -1,56 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css"
+import "./index.css";
+import Login from "./paginas/Login/Login";
 
-function Link(props){
-    return (
-        <a className="link" href={props.url}>
-            {props.children}
-        </a>
-    );
-};
 
-function Botao(props){
-    let classes = "botao";
-
-    if (props.desabilitado){
-        classes += " botao--desabilitado";
-    };
-
-    return (
-        <input className={classes} type="submit" value={props.children} />
-    );
-};
-
-const paginaLogin = (
-    <main className="pagina-login">
-        <h1>Login</h1>
-        <p>Entre com seu e-mail e senha.</p>
-        <Link url="/conta">Criar uma conta</Link>
-        <Link url="/login">Fazer login</Link>
-        <Botao desabilitado>Enviar</Botao>
-        <Botao>Enviar</Botao>
-    </main>
-);
-
-const props = {url: "/conta", children: "Criar uma conta"}
 const divisaoProjeto = document.getElementById("projeto");
-ReactDOM.render(paginaLogin, divisaoProjeto)
 
-// const linkCriarUmaConta = <a class="link" href="/conta">Criar uma conta</a>;
-// const linkFazerLogin = <a class="link" href="/conta">Fazer login</a>;
-
-// const formularioLogin = (
-//     <form>
-//         {[linkCriarUmaConta, linkFazerLogin]}
-//     </form>
-// );
-
-// const divisaoProjeto = document.getElementById("projeto");
-
-// ReactDOM.render(formularioLogin, divisaoProjeto)
-
-
+ReactDOM.render(<Login/>, divisaoProjeto)
 
 
 
